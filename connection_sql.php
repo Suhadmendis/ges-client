472ce5871fc0307969a7ad2aa26114ae0ee23be4
Suhadmendis <suhad.a.mendis@gmail.com>
2020-05-28 12:19:44 +0530

    up

--- connection_sql.php
+++ connection_sql.php
@@ -6,11 +6,17 @@
  * and open the template in the editor.
  */
 
-$servername = 'localhost';
-$username = 'root';
-$password = '';
+// $servername = 'localhost';
+// $username = 'root';
+// $password = '';
+// $port = 10060;
+// $dbname = 'ges';
+
+$servername = '162.144.37.131';
+$username = 'gihelk';
+$password = 's98hmXo95X';
 $port = 10060;
-$dbname = 'ges';
+$dbname = 'gihelk_gihe';
 
 
 $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
